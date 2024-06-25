75b6e3b734584363842a972791f7b755073bfed3
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-25 04:25:47 -0400

    Set up adaptive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
 
 
     return (
-        <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235px] rounded-b-[20px] pt-8 px-6">
+        <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235px] rounded-b-[20px] pt-8 px-6 mb-[1000px] md:mb-[480px] xl:mb-[200px]">
             <h1 className="text-Very-Dark-Blue font-bold text-2xl mb-1 dark:text-Very-Pale-Blue">Social Media Dashboard</h1>
             <p className="text-Dark-Grayish-Blue font-bold mb-6 dark:text-Desaturated-Blue">Total Followers: 23,004</p>
             <hr className="bg-black mb-[19px]" />
